0bc8635d4cad33e77ca9e8035ddbc949c4616820
yunusemreyigit <yunustc5461@gmail.com>
2025-07-02 14:32:43 +0300

    window creation

---
+++ include/Window.h
@@ -0,0 +1,22 @@
+//
+// Created by yey on 02.07.2025.
+//
+
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include <iostream>
+
+class Window {
+private:
+    int WIDTH = 600, HEIGHT = 800;
+    GLFWwindow* window;
+public:
+    Window(int width, int height);
+    GLFWwindow* getWindow();
+    bool shouldClose();
+
+};
+#endif //WINDOW_H
---
+++ src/Window.cpp
@@ -0,0 +1,33 @@
+//
+// Created by yey on 02.07.2025.
+//
+#include "Window.h"
+
+Window::Window(int width, int height): WIDTH(width), HEIGHT(height) {
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  window = glfwCreateWindow(WIDTH, HEIGHT, "Particle System", NULL, NULL);
+  if (!window) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+  }
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window,
+    [](GLFWwindow* window, const int width, const int height) { glViewport(0,0,width,height); });
+
+  if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+     std::cout << "Failed to initialize GLAD" << std::endl;
+  }
+  glViewport(0, 0, WIDTH, HEIGHT);
+}
+
+GLFWwindow* Window::getWindow() {
+  return window;
+}
+
+bool Window::shouldClose() {
+  return glfwWindowShouldClose(window);
+}
